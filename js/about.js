fc06b2dc139ead165e49e8573508d27df2033e31
rspaner <rpaner@ucsd.edu>
2022-12-05 01:24:33 -0800

    added about page

---
+++ js/about.js
@@ -0,0 +1,30 @@
+var original = "../images/heart.png";
+
+
+(function () {
+  var images = {
+   
+    "1": "../images/about/sculpting.png",
+    "2": "../images/about/jewelry_making.png",
+    "3": "../images/about/crochet.png"
+  };
+  var heart = document.getElementById("empty-heart");
+  var arr = document.getElementsByClassName('fill');
+  // console.log(arr)
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].onmouseover = function (e) {
+      var a = e.target;
+      var imgId = a.getAttribute('data-image-id');
+      var imgSrc = images[imgId];
+      heart.src = imgSrc;
+      heart.setAttribute("style", "width: 100%; height: auto");
+
+    } 
+    arr[i].onmouseout = function (){
+    heart.src = "../images/about/heart-dark.png";
+    }
+   
+  }
+
+})
+();
